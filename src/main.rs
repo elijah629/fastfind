9cbec5309b244325440a45ad50086639d6c8a160
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-10-07 17:41:39 -0700

    bump: 0.1.4, clap and progress

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,118 @@
-use std::{path::Path, fs::{File, self}, io::{BufWriter, Write}};
-
+use clap::{Parser, Subcommand};
+use indicatif::{ProgressBar, ProgressStyle};
+use std::{
+    fs::{self, File},
+    io::{prelude::*, BufReader, BufWriter, Write},
+    path::PathBuf,
+};
 use walkdir::WalkDir;
 
-const USAGE: &str = r"Super fast file indexer
+#[derive(Parser, Debug)]
+#[command(version, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-Usage: indexer [INDEX_DIRECTORY] [INDEX_OUTPUT]
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Creates an index for a directory and puts it in output
+    Index {
+        /// Directory to index
+        directory: PathBuf,
 
-Options:
-  -h, --help        Print help
-  -V, --version     Print version info and exit
-";
+        /// Output for the index.
+        output: PathBuf,
 
-#[inline]
-fn usage() -> ! {
-    eprintln!("{USAGE}");
-    std::process::exit(1);
+        #[arg(short, long)]
+        no_progress: bool,
+    },
+
+    /// Searches a sorted generated index.
+    Search {
+        /// Path for the generated index
+        index: PathBuf,
+
+        // Search string
+        search: String,
+    },
 }
 
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
-    let dir = match args.get(1) {
-        Some(x) => x,
-        None => usage(),
+    let args = Args::parse();
+
+    main_r(args).unwrap_or_else(|x| eprintln!("error: {}", x));
+}
+
+fn main_r(args: Args) -> Result<(), String> {
+    match args.command {
+        Commands::Index {
+            directory,
+            output,
+            no_progress,
+        } => {
+            if !directory.exists() {
+                return Err(format!("{directory:?} does not exist"));
+            }
+
+            if output.exists() {
+                println!("warn: index file already exists, removing");
+                fs::remove_file(&output).map_err(|_| "failed to remove index")?
+            }
+
+            let mut output =
+                BufWriter::new(File::create(&output).map_err(|_| "failed to create index")?);
+
+            let walker = WalkDir::new(directory)
+                .same_file_system(true)
+                .min_depth(1)
+                .into_iter()
+                .filter_map(|x| x.ok());
+
+            if no_progress {
+                for entry in walker {
+                    let data = format!("{}\n", entry.path().display());
+
+                    output
+                        .write_all(data.as_bytes())
+                        .map_err(|_| "failed to write to index")?;
+                }
+            } else {
+                let bar = ProgressBar::new(0).with_style(
+                    ProgressStyle::default_bar()
+                        .template("{wide_bar} {pos}/{len} {eta}")
+                        .unwrap(),
+                );
+
+                for entry in walker {
+                    let data = format!("{}\n", entry.path().display());
+                    output
+                        .write_all(data.as_bytes())
+                        .map_err(|_| "failed to write to index")?;
+                    bar.inc(1);
+
+                    let position = bar.position();
+                    if bar.position() >= bar.length().unwrap() {
+                        bar.set_length((position * 2) + (position / 2))
+                    }
+                }
+
+                bar.finish();
+            }
+        }
+
+        Commands::Search { index, search } => {
+            let files = BufReader::new(File::open(index).map_err(|_| "failed to open index")?);
+
+            for file in files.lines() {
+                if let Ok(file) = file {
+                    if file.contains(&search) {
+                        println!("{}", file);
+                    }
+                }
+            }
+        }
     };
 
-    let output = match args.get(2) {
-        Some(x) => x,
-        None => usage(),
-    };
-
-    if args.contains(&"-h".to_string()) || args.contains(&"--help".to_string()) {
-        usage();
-    }
-
-    if args.contains(&"-V".to_string()) || args.contains(&"--version".to_string()) {
-        println!("fastfind v{}", env!("CARGO_PKG_VERSION"));
-        return;
-    }
-    
-    let dir = Path::new(dir);
-
-    if !dir.exists() {
-        eprintln!("error: {dir:?} does not exist"); 
-        return;
-    }
-
-    if Path::new(output).exists() {
-        println!("warn: index file already exists, removing");
-        fs::remove_file(output).expect("error: failed to remove index");
-    }
-
-    let mut output = BufWriter::new(File::create(output).unwrap());
-
-    for entry in WalkDir::new(dir).same_file_system(true).min_depth(1).into_iter().filter_map(|x| x.ok()) {
-            let data = format!("{}\n", entry.path().display());
-            output.write_all(data.as_bytes()).expect("error: write error");
-    }
+    Ok(())
 }
